7c8b12182b4df17914b02a522da08fe674a324b2
Gibson Uwhetu <vellagibbs@gmail.com>
2023-09-30 19:24:49 +0100

    Added Layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SPacelet MarketPlace</title>
+    
+    <!-- Include Bootstrap CSS -->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    
+    <!-- Include Your Custom CSS (if any) -->
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <!-- Navbar (Bootstrap Navbar Example) -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">Space Let Marketplace</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Listings</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Profile</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main class="container mt-4">
+        @yield('content')
+    </main>
+
+    <!-- Include Bootstrap JavaScript (optional) -->
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+
+    <!-- Include Your Custom JavaScript (if any) -->
+    <script src="{{ asset('js/custom.js') }}"></script>
+</body>
+</html>
