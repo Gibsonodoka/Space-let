7c7987c9603de9c0d638f83b4af2305ca6cf53f9
Gibson Uwhetu <vellagibbs@gmail.com>
2023-10-02 10:37:39 +0100

    added logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,9 @@
         <!-- Navbar (Bootstrap Navbar Example) -->
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
-                <a class="navbar-brand" href="{{ route('home') }}">SpaceLet Marketplace</a>
+                <a class="navbar-brand" href="{{ route('home') }}">
+                    <img src="{{ asset('images/logo/spacelet-logo.png') }}" alt="SpaceLet Logo">
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
