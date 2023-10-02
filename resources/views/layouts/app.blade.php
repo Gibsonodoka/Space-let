be06d4c41432c60629dc48a51decd07af2d5f7ac
Gibson Uwhetu <vellagibbs@gmail.com>
2023-10-02 10:52:11 +0100

    logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
                 <a class="navbar-brand" href="{{ route('home') }}">
-                    <img src="{{ asset('images/logo/spacelet-logo.png') }}" alt="SpaceLet Logo" class="logo-img>
+                    <img src="{{ asset('images/logo/spacelet-logo.png') }}" alt="SpaceLet Logo" class="logo-img">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
