57d243da199e49e75758a84ee150329c136ff3b2
Gibson Uwhetu <100386202+Gibsonodoka@users.noreply.github.com>
2023-11-11 22:07:39 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -35,7 +35,7 @@ require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
-| Run The Application
+| Run The Application with the given server port 
 |--------------------------------------------------------------------------
 |
 | Once we have the application, we can handle the incoming request using
